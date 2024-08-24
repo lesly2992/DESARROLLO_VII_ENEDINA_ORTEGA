8e6dd09e006a232810fc28ddcdf5b52b04911bd4
unknown <leslyortg29@gmail.com>
2024-08-23 23:08:44 -0500

    Ejercicio práctico: combinación de conceptos aprendidos

--- TALLERES/TALLERES_2/index.php
+++ TALLERES/TALLERES_2/index.php
@@ -45,6 +45,31 @@ printf("Me llamo %s y tengo %d años<br>", $nombre, $edad);
 var_dump($nombre);
 echo "<br>";
 
+
+echo"<br>";
+$nombre = "María";
+$edad = 30;
+$ciudad = "Madrid";
+
+define("PROFESION", "Ingeniera");
+
+$mensaje1 = "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años.";
+$mensaje2 = "Vivo en $ciudad y soy " . PROFESION . ".";
+
+echo $mensaje1 . "<br>";
+print($mensaje2 . "<br>");
+
+printf("En resumen: %s, %d años, %s, %s<br>", $nombre, $edad, $ciudad, PROFESION);
+
+echo "<br>Información de debugging:<br>";
+var_dump($nombre);
+echo "<br>";
+var_dump($edad);
+echo "<br>";
+var_dump($ciudad);
+echo "<br>";
+var_dump(PROFESION);
+echo "<br>";
                     
 
 ?>
