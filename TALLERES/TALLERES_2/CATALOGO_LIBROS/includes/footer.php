dfa20f14bfab9776bf14a69804d0c5adef6074ce
lesly2992 <91107710+lesly2992@users.noreply.github.com>
2024-09-16 18:50:21 -0500

    Delete TALLERES directory

--- TALLERES/TALLERES_2/CATALOGO_LIBROS/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-        <p>&copy; <?php echo date("Y"); ?> Catálogo de Libros. Todos los derechos reservados.</p>
-    </footer>
-</body>
-</html>
--- TALLERES/TALLERES_2/SISTEMA_PLANTILLAS/plantillas/pie_pagina.php
+++
@@ -1,6 +0,0 @@
-</main>
-    <footer>
-        <p>&copy; <?php echo date('Y'); ?> Mi Sitio Web. Todos los derechos reservados.</p>
-    </footer>
-</body>
-</html>
